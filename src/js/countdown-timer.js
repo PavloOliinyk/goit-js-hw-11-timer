e4dc503bbbd8ff26ec1f87ba33c971ca69e0cd33
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-07-21 19:21:19 +0300

    сделал таймер со стилями

--- src/js/countdown-timer.js
+++ src/js/countdown-timer.js
@@ -1,17 +1,16 @@
 class CountdownTimer {
   timerId;
   selector;
+  timerRef;
 
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
+    this.timerRef = document.querySelector(this.selector);
   }
 
   makeMarkup() {
-    const div = document.createElement('div');
-    div.classList.add('timer');
-    div.id = this.selector;
-    div.innerHTML = `<div class='field'>
+    const markUp = `<div class='field'>
     <span class='value' data-value='days'>
       11
     </span>
@@ -39,26 +38,31 @@ class CountdownTimer {
     <span class='label'>Seconds</span>
   </div>`;
 
-    document.body.appendChild(div);
+    this.timerRef.innerHTML = markUp;
   }
 
-  runCountdownTimer() {
-    const deadline = this.targetDate;
-    const newTimerId = this.selector;
+  get Refs() {
+    const { timerRef } = this;
 
-    this.timerId = setInterval(this.updateCountdownTime, 1000, deadline, newTimerId);
-  }
-
-  updateCountdownTime(deadline, newTimerId) {
-    const timerWrapper = document.getElementById(`${newTimerId}`);
-    const time = deadline - Date.now();
-
-    const Refs = {
-      days: timerWrapper.querySelector('[data-value="days"]'),
-      hours: timerWrapper.querySelector('[data-value="hours"]'),
-      mins: timerWrapper.querySelector('[data-value="mins"]'),
-      secs: timerWrapper.querySelector('[data-value="secs"]'),
+    return {
+      days: timerRef.querySelector('[data-value="days"]'),
+      hours: timerRef.querySelector('[data-value="hours"]'),
+      mins: timerRef.querySelector('[data-value="mins"]'),
+      secs: timerRef.querySelector('[data-value="secs"]'),
     };
+  }
+
+  startTimer() {
+    this.makeMarkup();
+
+    const Refs = this.Refs;
+    const deadline = this.targetDate;
+
+    this.timerId = setInterval(this.updateCountdownTime, 1000, deadline, Refs);
+  }
+
+  updateCountdownTime(deadline, Refs) {
+    const time = deadline - Date.now();
 
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -82,11 +86,9 @@ class CountdownTimer {
   }
 }
 
-// Во время вызова экземпляра класса прописать selector: '#timer'
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 31, 2021'),
+  targetDate: new Date('Jul 25, 2021'),
 });
 
-timer.makeMarkup();
-timer.runCountdownTimer();
+timer.startTimer();
